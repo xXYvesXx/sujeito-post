67460aa15e2d8ae7dd9c87e7b430e0004537c82b
xXYvesXx <yvesh20@gmail.com>
2020-10-06 18:45:07 -0300

    criação dos buttons e estilização

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="header"></div>
+  <header id="header">
+    <button>SujeitoPost</button>
+    <button>Dashboard</button>
+  </header>
 </template>
 
 <script>
@@ -8,5 +11,32 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+#header {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 60px;
+  background-color: #202225;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
+
+#header a {
+  text-decoration: none;
+  font-size: 30px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: bold;
+}
+
+button {
+  cursor: pointer;
+  border: 0;
+  color: #fff;
+  background: transparent;
+  padding: 5px 15px;
+  outline: none;
+  font-weight: bold;
+}
 </style>
