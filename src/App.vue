1400f6adb705abad125b58118addaa00c9506578
xXYvesXx <yvesh20@gmail.com>
2020-10-06 18:38:04 -0300

    criação do Header

--- src/App.vue
+++ src/App.vue
@@ -9,13 +9,12 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="header"></div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+</style>
