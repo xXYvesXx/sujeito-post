082191602d12871f2ba61547f3bda2b7fd74caf9
xXYvesXx <yvesh20@gmail.com>
2020-10-08 18:26:25 -0300

    implementando a função de submit

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,105 +1,128 @@
 <template>
   <div id="login">
-      
-      <div class="loginArea" v-if="login">
-        <h1>Entrar</h1>
-        <form>
-          <input type="text" placeholder="email@email.com" v-model="email" />
-          <input type="password" placeholder="Sua senha..." v-model="password" />
-          <button type="submit">Acessar</button>
-        </form>
-        <a @click="toggleBtn" >Criar uma conta</a>
-      </div>
+    <div class="loginArea" v-if="login">
+      <h1>Entrar</h1>
+      <form>
+        <input type="text" placeholder="email@email.com" v-model="email" />
+        <input type="password" placeholder="Sua senha..." v-model="password" />
+        <button type="submit">Acessar</button>
+      </form>
+      <a @click="toggleBtn">Criar uma conta</a>
+    </div>
 
-      <div class="loginArea" v-else>
-        <h1>Cadastrar</h1>
-        <form>
-          <input type="text" placeholder="Nome" v-model="nome" />
-          <input type="text" placeholder="email@email.com" v-model="email" />
-          <input type="password" placeholder="Sua senha..." v-model="password" />
-          <button type="submit">Cadastrar</button>
-        </form>
-        <a @click="toggleBtn">Já possuo uma conta</a>
-      </div>
-
+    <div class="loginArea" v-else>
+      <h1>Cadastrar</h1>
+      <form @submit.prevent="handleRegister">
+        <input type="text" placeholder="Nome" v-model="nome" />
+        <input type="text" placeholder="email@email.com" v-model="email" />
+        <input type="password" placeholder="Sua senha..." v-model="password" />
+        <button type="submit">Cadastrar</button>
+      </form>
+      <a @click="toggleBtn">Já possuo uma conta</a>
+    </div>
   </div>
 </template>
 
 <script>
-
+import firebase from "../services/firebaseConnection";
 export default {
-  name: 'Login',
-  data(){
-    return{
-      nome: '',
-      email: '',
-      password: '',
+  name: "Login",
+  data() {
+    return {
+      nome: "",
+      email: "",
+      password: "",
       login: true,
-    }
+    };
   },
-  methods:{
-    toggleBtn(){
+  methods: {
+    toggleBtn() {
       this.login = !this.login;
-      this.nome = '';
-      this.email = '';
-      this.password = '';
-    }
-  }
+      this.nome = "";
+      this.email = "";
+      this.password = "";
+    },
 
-}
+    async handleRegister() {
+      const { user } = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password);
+
+      await firebase
+        .firestore()
+        .collection("users")
+        .doc(
+          user.uid.set({
+            nome: this.nome,
+          })
+        )
+        .then(async () => {
+          const usuarioLogado = {
+            uid: user.uid,
+            nome: this.nome,
+          };
+
+          await localStorage.setItem("devpost", JSON.stringify(usuarioLogado));
+        })
+        .catch(() => {
+          console.log("Erro ao cadastrar");
+        });
+
+      this.$router.push("/");
+    },
+  },
+};
 </script>
 
 <style scoped>
-  h1{
-    text-align: center;
-    color: #FFF;
-  }
+h1 {
+  text-align: center;
+  color: #fff;
+}
 
-  .loginArea{
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    padding: 20px;
-    margin-top: 65px;
-    max-width: 600px;
-    background: #4c5059;
-    border-radius: 5px;
-  }
+.loginArea {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding: 20px;
+  margin-top: 65px;
+  max-width: 600px;
+  background: #4c5059;
+  border-radius: 5px;
+}
 
-  form{
-    display: flex;
-    flex-direction: column;
-    margin: 0px 25px;
-  }
+form {
+  display: flex;
+  flex-direction: column;
+  margin: 0px 25px;
+}
 
-  input{
-    margin-bottom: 10px;
-    height: 30px;
-    width: 300px;
-    font-size: 18px;
-    padding: 10px;
-    outline: none;
-    border:0;
-    background: rgb(241,241,241);
-  }
+input {
+  margin-bottom: 10px;
+  height: 30px;
+  width: 300px;
+  font-size: 18px;
+  padding: 10px;
+  outline: none;
+  border: 0;
+  background: rgb(241, 241, 241);
+}
 
-  button{
-    height: 35px;
-    border:0;
-    background-color: #7289DA;
-    color: #FFF;
-    font-size: 16px;
-    font-weight: 600;
-    outline: none;
-    cursor: pointer;
-  }
+button {
+  height: 35px;
+  border: 0;
+  background-color: #7289da;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 600;
+  outline: none;
+  cursor: pointer;
+}
 
-  .loginArea a{
-    margin-top: 20px;
-    text-decoration: none;
-    color: #FFF;
-    cursor: pointer;
-  }
-
-
+.loginArea a {
+  margin-top: 20px;
+  text-decoration: none;
+  color: #fff;
+  cursor: pointer;
+}
 </style>
