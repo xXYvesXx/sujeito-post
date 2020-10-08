1b271fb5b3ed329f4bcf10b931a3b7c263e41ae6
xXYvesXx <yvesh20@gmail.com>
2020-10-08 18:04:13 -0300

    configuração das rotas com requisitem autenticação

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from './services/firebaseConnection'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: (h) => h(App),
-}).$mount('#app')
+let app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: (h) => h(App),
+    }).$mount('#app')
+  }
+})
--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import firebase from './services/firebaseConnection'
+
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
@@ -14,20 +16,42 @@ const router = new Router({
     {
       path: '/',
       component: Home,
+      meta: {
+        requiresAuth: true, //requer que esteja autenticado
+      },
     },
     {
       path: '/dashboard',
       component: Dashboard,
+      meta: {
+        requiresAuth: true, //requer que esteja autenticado
+      },
     },
     {
       path: '/perfil/:userid',
       component: Perfil,
+      props: true,
+      meta: {
+        requiresAuth: true, //requer que esteja autenticado
+      },
     },
     {
       path: '/login',
       component: Login,
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth)
+
+  if (requiresAuth && !firebase.auth().currentUser) {
+    next('/login')
+  } else {
+    next()
+  }
+
+  //requiresAuth = false
 })
 
 export default router
